a08bca97c601cd9c119637d2d87738cc8dd320b6
OtaOkpogo <otaokpogo@yahoo.com>
2024-05-27 14:38:26 -0700

    Updated console.py

--- console.py
+++ console.py
@@ -26,7 +26,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """ overwriting the emptyline method """
+        """ for overwriting the emptyline method """
         return False
 
     def do_quit(self, arg):
